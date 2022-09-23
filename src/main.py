b546029814659b8773260eb15db4523b658332b8
VIDAL-Antoine <avidal78390@gmail.com>
2022-09-23 22:47:20 +0200

    France graphe fait

--- src/main.py
+++ src/main.py
@@ -16,29 +16,24 @@ suppress_qt_warnings()
 #Pour Windows
 
 
-
-
-
-
 G = nx.Graph()
-
 
 #Constructions des sommets
 df_towns_coords = pd.read_csv("towns_coords.csv")
 for row in df_towns_coords.itertuples():
-    print(f'{row.town}, {row.x}, {row.y}')
     G.add_node(row.town, pos=(row.x, row.y))
-
 pos=nx.get_node_attributes(G,'pos')
-
-
 
 #Consutrctions des arêtes
 df_towns_edges = pd.read_csv("towns_edges_2d_array.csv")
-
-
-
+df_towns_edges = df_towns_edges.set_index("Town")
+for rowIndex, row in df_towns_edges.iterrows(): #iterate over rows
+    for columnIndex, value in row.items():
+        if value > 0:
+            G.add_edge(rowIndex, columnIndex, weight=value)
+labels = nx.get_edge_attributes(G,'weight')
 
 nx.draw(G, pos, with_labels=True)
-plt.savefig("gps.png")
+#nx.draw_networkx_edge_labels(G, pos, edge_labels=labels) #Afficher les poids des arêtes
+plt.savefig("france_graphe.png")
 plt.show()
