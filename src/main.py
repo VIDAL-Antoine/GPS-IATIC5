b277c938ae9e72cd76df625a085715ae73c8921f
VIDAL-Antoine <avidal78390@gmail.com>
2022-09-25 14:53:17 +0200

    Ajout arêtes plus court chemin

--- src/main.py
+++ src/main.py
@@ -1,17 +1,14 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 import pandas as pd
-from collections import defaultdict
 
 #Pour Windows
 from os import environ
-
 def suppress_qt_warnings():
     environ["QT_DEVICE_PIXEL_RATIO"] = "0"
     environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
     environ["QT_SCREEN_SCALE_FACTORS"] = "1"
     environ["QT_SCALE_FACTOR"] = "1"
-
 suppress_qt_warnings()
 #Pour Windows
 
@@ -24,7 +21,7 @@ for row in df_towns_coords.itertuples():
     G.add_node(row.town, pos=(row.x, row.y))
 pos=nx.get_node_attributes(G,'pos')
 
-#Consutrctions des arêtes
+#Constructions des arêtes
 df_towns_edges = pd.read_csv("towns_edges_2d_array.csv")
 df_towns_edges = df_towns_edges.set_index("Town")
 for rowIndex, row in df_towns_edges.iterrows(): #iterate over rows
@@ -33,7 +30,17 @@ for rowIndex, row in df_towns_edges.iterrows(): #iterate over rows
             G.add_edge(rowIndex, columnIndex, weight=value)
 labels = nx.get_edge_attributes(G,'weight')
 
-nx.draw(G, pos, with_labels=True)
+#Détermination du plus court chemin
+shortest_path = nx.dijkstra_path(G, "Lyon", "Rennes")
+shortest_path_edges = [(shortest_path[i],shortest_path[i+1]) for i in range(len(shortest_path)-1)]
+
+edge_color_list = ["black"]*len(G.edges)
+for i, edge in enumerate(G.edges()):
+    if edge in shortest_path_edges or (edge[1],edge[0]) in shortest_path_edges:
+        edge_color_list[i] = "red"
+
+
+nx.draw(G, pos, with_labels=True, edge_color=edge_color_list)
 #nx.draw_networkx_edge_labels(G, pos, edge_labels=labels) #Afficher les poids des arêtes
 plt.savefig("france_graphe.png")
 plt.show()
