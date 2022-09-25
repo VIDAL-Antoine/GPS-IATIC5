d29e8501b32d6d5183f0b319c165049970dc0f6a
VIDAL-Antoine <avidal78390@gmail.com>
2022-09-25 15:42:50 +0200

    Ajout tkinter fenêtre basique d'affichage d'un png

--- src/main.py
+++ src/main.py
@@ -1,17 +1,16 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 import pandas as pd
+from tkinter import *
+from PIL import ImageTk, Image
 
-#Pour Windows
-from os import environ
 def suppress_qt_warnings():
+    from os import environ
     environ["QT_DEVICE_PIXEL_RATIO"] = "0"
     environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
     environ["QT_SCREEN_SCALE_FACTORS"] = "1"
     environ["QT_SCALE_FACTOR"] = "1"
-suppress_qt_warnings()
-#Pour Windows
-
+suppress_qt_warnings() #Pour Windows
 
 G = nx.Graph()
 
@@ -19,7 +18,7 @@ G = nx.Graph()
 df_towns_coords = pd.read_csv("towns_coords.csv")
 for row in df_towns_coords.itertuples():
     G.add_node(row.town, pos=(row.x, row.y))
-pos=nx.get_node_attributes(G,'pos')
+pos=nx.get_node_attributes(G,"pos")
 
 #Constructions des arêtes
 df_towns_edges = pd.read_csv("towns_edges_2d_array.csv")
@@ -28,10 +27,10 @@ for rowIndex, row in df_towns_edges.iterrows(): #iterate over rows
     for columnIndex, value in row.items():
         if value > 0:
             G.add_edge(rowIndex, columnIndex, weight=value)
-labels = nx.get_edge_attributes(G,'weight')
+labels = nx.get_edge_attributes(G,"weight")
 
 #Détermination du plus court chemin
-shortest_path = nx.dijkstra_path(G, "Lyon", "Rennes")
+shortest_path = nx.dijkstra_path(G, "Bourges", "Lille")
 shortest_path_edges = [(shortest_path[i],shortest_path[i+1]) for i in range(len(shortest_path)-1)]
 
 edge_color_list = ["black"]*len(G.edges)
@@ -43,4 +42,13 @@ for i, edge in enumerate(G.edges()):
 nx.draw(G, pos, with_labels=True, edge_color=edge_color_list)
 #nx.draw_networkx_edge_labels(G, pos, edge_labels=labels) #Afficher les poids des arêtes
 plt.savefig("france_graphe.png")
-plt.show()
+#plt.show()
+
+root = Tk()
+img = ImageTk.PhotoImage(Image.open("france_graphe.png"))
+panel = Label(root, image = img)
+panel.pack()
+b = Button(root,text="Supprimer image",command=lambda:panel.destroy())
+b.pack()
+
+root.mainloop()
